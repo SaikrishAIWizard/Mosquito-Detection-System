20c3a43405d7fdede317a82129fbd3fb430b0ef1
Saikrish <vijayasatyad@gmail.com>
2026-04-19 13:23:06 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="mosquito-detector",
     version="0.1.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Saikrish",
+    author_email="vijayasatyad@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
